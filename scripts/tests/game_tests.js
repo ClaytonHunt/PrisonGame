e0917008bdf0666dbfca193af3856df7d1bebfae
Clayton Hunt <claytonhunt_104@hotmail.com>
2013-09-17 12:48:09 -0400

    Added tests for Prison Game Main

--- scripts/tests/game_tests.js
+++ scripts/tests/game_tests.js
@@ -1,2 +1,45 @@
+var Prison = Prison || {};
+Prison.Game = function() {
+    var self = this;
 
-https://github.com/ClaytonHunt/PrisonGame.git
+    self.areas = null;
+
+    self.addArea = function(area) {
+        self.areas = area;
+    };
+
+    return self;
+};
+
+describe("Prison Game", function() {
+    var game;
+
+    beforeEach(function() {
+        game = new Prison.Game();
+    });
+
+    it("Exists", function(){
+        expect(game).not.toBe(null);
+    });
+
+    it("Is a Prison Game", function() {
+        expect(game instanceof Prison.Game).toBeTruthy();
+    });
+
+    describe("Area", function() {
+        it("can be added", function() {
+            var area = "";
+            game.addArea(area);
+
+            expect(game.areas).not.toBe(null);
+        });
+
+        it("Can have multiple Areas", function() {
+            var area1 = "1";
+            var area2 = "2";
+            game.addArea(area1);
+            game.addArea(area2);
+
+        });
+    });
+});
