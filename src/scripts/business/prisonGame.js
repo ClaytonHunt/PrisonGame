29bce06c9d8677657690db925970bbcff7c04662
Clayton Hunt <claytonhunt_104@hotmail.com>
2013-09-18 13:07:48 -0400

    Added tests, pulled prison out of test file

--- src/scripts/business/prisonGame.js
+++ src/scripts/business/prisonGame.js
@@ -1,1 +1,38 @@
+var Prison = Prison || {};
 
+Prison.Block = function () {
+    "use strict";
+    var self = this;
+
+    return self;
+};
+
+Prison.Area = function () {
+    "use strict";
+    var self = this;
+    var __ = {};
+
+    self.block = [];
+
+    self.addBlock = function (Block) {
+        if (!Block || !(Block instanceof Prison.Block))
+            throw 'Argument type incorrect use Prison.Block.';
+
+        self.block.push(Block);
+    };
+
+    return self;
+};
+
+Prison.Game = function () {
+    "use strict";
+    var self = this;
+
+    self.areas = [];
+
+    self.addArea = function (area) {
+        self.areas.push(area);
+    };
+
+    return self;
+};
